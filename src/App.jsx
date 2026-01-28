50a732221a23ebd40e6c52dc0d812fa094942b9b
Luca Giampieri <luca.giampieri95@gmail.com>
2026-01-28 14:11:11 +0100

    Pulizia e settaggio iniziale

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+//import delle rotte da reat-router-dom
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+//import delle pages da inserire nelle rotte
+import HomePage from "./pages/HomePage"
+import AboutUs from "./pages/AboutUs"
+import Products from "./pages/Products"
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
