8a8cecabfef0ce900792d1542cc939cd239596d4
Luca Giampieri <luca.giampieri95@gmail.com>
2026-01-28 15:46:17 +0100

    Aggiunta HomePage e  AboutUs

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -6,6 +6,10 @@ function AboutUs() {
     return (
         <>
             <NavBar />
+            <div className="container">
+                <h2 className="title"> Chi siamo</h2>
+                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatem animi repudiandae blanditiis, obcaecati nostrum, vitae neque sapiente molestias ipsum accusamus nihil tempora aspernatur rem omnis architecto dignissimos officia in. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nulla expedita officiis unde itaque delectus placeat laborum accusantium maxime quos, et ut voluptates, ea nostrum, optio in soluta. Culpa, error! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit earum quasi odit expedita provident, facilis itaque? Vel saepe recusandae, eius unde obcaecati fugiat, qui nemo error deserunt cupiditate et harum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iusto dolores, similique repellat odit nesciunt architecto repudiandae ea impedit expedita illo, aliquid natus repellendus tempore tempora veritatis velit recusandae alias.</p>
+            </div>
         </>
     )
 }
